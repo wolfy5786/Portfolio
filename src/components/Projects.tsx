a225550815f3941f473ea4b603183222eb31f156
Ankush Makhijani <makhijaniankush24@gmail.com>
2026-02-26 12:07:52 -0800

    minor ui changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,19 +3,17 @@ import { ImageWithFallback } from './figma/ImageWithFallback';
 
 const projects = [
   {
-    title: 'Poseidon',
+    title: 'Boost',
     description: 'Automated Software Testing and QA',
     image: 'https://images.unsplash.com/photo-1763568258244-9d5aa9c3ce45?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzb2Z0d2FyZSUyMHRlc3RpbmclMjBhdXRvbWF0aW9uJTIwY29kZXxlbnwxfHx8fDE3NjUwMDQ5MTJ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['Python', 'FastAPI', 'PostgreSQL','PyUint','Docker','Jenkins', 'Linux'],
-    liveUrl: '#',
-    githubUrl: 'https://github.com/wolfy5786/Poseidon',
+    githubUrl: 'https://github.com/wolfy5786/boost',
   },
   {
     title: 'Onilne Multiplayer Poker',
     description: 'Web Based Online Multiplayer Poker',
     image: 'https://images.unsplash.com/photo-1560327317-031f46d0e995?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwb2tlciUyMGNhcmRzJTIwZ2FtZXxlbnwxfHx8fDE3NjUwMDQ5MTN8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['Python', 'Reddis', 'Django'],
-    liveUrl: '#',
     githubUrl: 'https://github.com/wolfy5786/Online_Poker',
   },
   {
@@ -23,7 +21,6 @@ const projects = [
     description: 'Cab Booking REST Service',
     image: 'https://images.unsplash.com/photo-1619933557175-9168d5209fee?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx0YXhpJTIwY2FiJTIwcmlkZXxlbnwxfHx8fDE3NjUwMDQ5MTN8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['Java', 'Spring Boot', 'PostgresSQL','AWS EC2','Docker'],
-    liveUrl: '#',
     githubUrl: 'https://github.com/wolfy5786/piggy_back',
   },
   {
@@ -31,7 +28,6 @@ const projects = [
     description: 'Test Cricket Analytics and Simulation',
     image: 'https://images.unsplash.com/photo-1635127725453-5f6ad32cbf92?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjcmlja2V0JTIwc3BvcnQlMjBzdGFkaXVtfGVufDF8fHx8MTc2NTAwNDkxM3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['Python', 'Gausian Discriminant Analysis','PowerBI'],
-    liveUrl: '#',
     githubUrl: 'https://github.com/wolfy5786/Cricket_analysis',
   },
   {
@@ -39,7 +35,6 @@ const projects = [
     description: 'Game Recommendation System',
     image: 'https://images.unsplash.com/photo-1644571580854-114d7d8fa383?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxnYW1pbmclMjBjb250cm9sbGVyJTIwY29uc29sZXxlbnwxfHx8fDE3NjUwMDQ5MTR8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['TypeScript', 'Node.js','AWS EC2'],
-    liveUrl: '#',
     githubUrl: 'https://github.com/wolfy5786/NextPlay',
   },
     {
@@ -47,7 +42,6 @@ const projects = [
     description: 'Breadth First Search on Website to mine Data',
     image: 'https://images.unsplash.com/photo-1605116188625-8522bf02d10f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx3ZWIlMjBjcmF3bGVyJTIwbmV0d29ya3xlbnwxfHx8fDE3NjUwMDQ5MTR8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
     tags: ['Python','curl', 'Linux', 'C'],
-    liveUrl: '#',
     githubUrl: 'https://github.com/wolfy5786/WebCrawler',
   },
 ];
@@ -90,14 +84,7 @@ export function Projects() {
                   ))}
                 </div>
                 
-                <div className="flex gap-4">
-                  <a 
-                    href={project.liveUrl}
-                    className="flex items-center gap-2 text-blue-600 hover:text-blue-700 transition-colors"
-                  >
-                    <ExternalLink className="w-4 h-4" />
-                    <span>Live Demo</span>
-                  </a>
+
                   <a 
                     href={project.githubUrl}
                     className="flex items-center gap-2 text-slate-600 hover:text-slate-700 transition-colors"
